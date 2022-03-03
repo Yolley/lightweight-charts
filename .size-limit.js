7caf41088d1de4e0804f30cbee2f168c93d980c4
Yolley <comingreal@gmail.com>
2022-03-03 03:39:36 +0300

    update size limit, add npm ignore

--- .size-limit.js
+++ .size-limit.js
@@ -4,11 +4,11 @@ module.exports = [
 	{
 		name: 'ESM',
 		path: 'dist/lightweight-charts.esm.production.js',
-		limit: '45 KB',
+		limit: '46.5 KB',
 	},
 	{
 		name: 'Standalone',
 		path: 'dist/lightweight-charts.standalone.production.js',
-		limit: '45.5 KB',
+		limit: '47 KB',
 	},
 ];
